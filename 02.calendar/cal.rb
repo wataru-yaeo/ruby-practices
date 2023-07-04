c8b9d8f2a686cd267f4cbaf8db57c82c80460a15
wataru-yaeo <wataru.yaeo@gmail.com>
2023-07-04 16:14:30 +0900

    レビューに対する修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -11,23 +11,21 @@ opt.parse!(ARGV, into: params)
 selected_year = params[:y].to_i
 selected_month = params[:m].to_i
 
-if params[:y] == nil
+if params[:y].nil?
   selected_year = Date.today.year
 end
-if params[:m] == nil
+if params[:m].nil?
   selected_month = Date.today.month
 end
 
 last_day = Date.new(selected_year, selected_month, -1) 
 first_day = Date.new(selected_year, selected_month, 1) 
 
-print "     #{selected_month}月 #{selected_year}","\n"
-print "日 月 火 水 木 金 土","\n"
+puts "     #{selected_month}月 #{selected_year}"
+puts "日 月 火 水 木 金 土"
 print " " * first_day.wday * 3
 
 (first_day..last_day).each do |x|
   print x.strftime("%e")," "
-  if x.saturday?
-    print "\n"
-  end
+  puts if x.saturday?
 end
