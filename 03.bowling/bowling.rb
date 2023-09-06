e5b1afc3e0792080f4bc31e9fc879921b215afdd
wataru-yaeo <wataru.yaeo@gmail.com>
2023-09-06 13:21:36 +0900

    10フレーム目の計算方法の修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -15,30 +15,21 @@ end
 
 frames = shots.each_slice(2).to_a { |s| }
 
-point1 = 0
-frames[0..8].each_with_index do |frame, index|
-  point1 += if frame[0] == 10 # 1〜9フレーム
-              if frames[index + 1][0] == 10 # 2連続ストライク
-                20 + frames[index + 2][0]
-              else # ストライク
-                10 + frames[index + 1].sum
-              end
-            elsif frame.sum == 10 # スペア
-              10 + frames[index + 1][0]
-            else
-              frame.sum
-            end
+point = 0
+frames[0..8].each_with_index do |frame, index| # 1〜9フレーム
+  point += if frame[0] == 10
+             if frames[index + 1][0] == 10 # 2連続ストライク
+               20 + frames[index + 2][0]
+             else # ストライク
+               10 + frames[index + 1].sum
+             end
+           elsif frame.sum == 10 # スペア
+             10 + frames[index + 1][0]
+           else
+             frame.sum
+           end
 end
 
-point2 = 0 # 10フレーム
-if frames[9][0] == 10 && frames[10][0] == 10
-  point2 = frames[9].sum + frames[10].sum + frames[11].sum
-elsif frames[9][0] == 10
-  point2 = frames[9].sum + frames[10].sum
-elsif frames[9].sum == 10 # スペア
-  point2 = frames[9].sum + frames[10].sum
-else
-  point2 += frames[9].sum
-end
+point += frames[9..].flatten.sum
 
-puts point1 + point2
+puts point
